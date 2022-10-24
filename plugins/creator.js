606bac12b23710cc55e3fd969cf9cc9ed640ad7d
nakroth09 <95112460+nakroth09@users.noreply.github.com>
2022-10-25 08:08:06 +0900

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -20,17 +20,17 @@ let handler  = async (m, { conn, command, args, usedPrefix, DevMode }) => {
 //------------ BIO
 let ppown = await conn.profilePictureUrl(nomorown + '@s.whatsapp.net', 'image').catch(_ => hwaifu[1]) 
 let teksbio = `${htki} *BIODATA* ${htka}
-${htjava} *💌 Nama* : WH-MODS-DEV
-${htjava} *✉️ Nama RL* : HAMBA ALLAH
-${htjava} *♂️ Gender* : Boys
+${htjava} *💌 Nama* : Rafi
+${htjava} *✉️ Nama RL* : Rafi
+${htjava} *♂️ Gender* : Boy
 ${htjava} *🕋 Agama* : Islam
-${htjava} *⏰ Tanggal lahir* : Private 🥶
-${htjava} *🎨 Umur* : 18
+${htjava} *⏰ Tanggal lahir* : gini
+${htjava} *🎨 Umur* : 13
 ${htjava} *🧮 Kelas* : SUDAH TAMAT
-${htjava} *🧩 Hobby* : MODIFIKASI WHATSAPP "MODDER WHATSAPP"
-${htjava} *💬 Sifat* : ASIK DAH KALAU DAH KENAL
-${htjava} *🗺️ Tinggal* : Indo, Depok, Jawa Barat
-${htjava} *❤️ Waifu* : -
+${htjava} *🧩 Hobby* : Gambar
+${htjava} *💬 Sifat* : Gitu
+${htjava} *🗺️ Tinggal* : Indonesia, Maluku Utara, Ternate
+${htjava} *❤️ Waifu* : uish banyak
 
 ${htjava} *📷 ɪɴsᴛᴀɢʀᴀᴍ* : ${sig}
 ${htjava} *🐈 ɢɪᴛʜᴜʙ:* ${sgh}
